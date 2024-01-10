066c06685d8943d0b43dc20675894a9758afa5d3
RomanShcherbatiuk <112552015+RomanShcherbatiuk@users.noreply.github.com>
2024-01-11 01:14:45 +0300

    Add files via upload

---
+++ Diploma/src/main/kotlin/DataClass.kt
@@ -0,0 +1,29 @@
+import java.time.OffsetDateTime
+
+data class DataClass(
+    val time: OffsetDateTime,
+    val dmea: Double,
+    val bpos: Double,
+    val hkla: Double,
+    val woba: Double,
+    val rpma: Double,
+    val tqa: Double,
+    val ropa: Double,
+    val dbtm: Double,
+    val spra: Double,
+    val spm1: Double,
+    val spm2: Double,
+    val mfia: Double,
+    val mfop: Double,
+    val tvt: Double,
+    val tv01: Double,
+    val tv02: Double,
+    val tv03: Double,
+    val tv04: Double,
+    val tv05: Double,
+    val mdia: Double,
+    val mdoa: Double,
+    val mtia: Double,
+    val mtoa: Double,
+    val gasa: Double
+)
